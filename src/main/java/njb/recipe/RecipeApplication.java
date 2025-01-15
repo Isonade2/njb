c34b44711e33af2a774faaa90bf24b0ba37c656e
leeyeonju02 <85239317+leeyeonju02@users.noreply.github.com>
2025-01-15 19:52:33 +0900

    chore : yaml 추가

--- src/main/java/njb/recipe/RecipeApplication.java
+++ src/main/java/njb/recipe/RecipeApplication.java
@@ -1,5 +1,6 @@
 package njb.recipe;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RecipeApplication {
 
 	public static void main(String[] args) {
+		// Load environment variables from .env file
+		Dotenv dotenv = Dotenv.configure().load();
 		SpringApplication.run(RecipeApplication.class, args);
 	}
 }
