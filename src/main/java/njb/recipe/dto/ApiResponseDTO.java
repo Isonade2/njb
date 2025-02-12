6ef5c951f1a233b092af7ab7bf3acf6ff20413ff
Isonade2 <c1004sos@naver.com>
2025-02-13 02:47:44 +0900

    feat: 공통 응답 DTO 및 메서드 작성

---
+++ src/main/java/njb/recipe/dto/ApiResponseDTO.java
@@ -0,0 +1,16 @@
+package njb.recipe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class ApiResponseDTO<T> {
+    private String status; // 응답 상태
+    private String message; // 응답 메시지
+    private T data; // data는 제네릭으로 받는다. 실제 데이터
+
+}
