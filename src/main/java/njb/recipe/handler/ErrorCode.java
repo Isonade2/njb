9bda3d0f972a76a83babed77cec5a7c2bdb1f3b7
Isonade2 <c1004sos@naver.com>
2025-04-26 02:41:50 +0900

    refactor: 도메인 폴더 추가, 멤버 관련 클래스 디렉토리 변경

---
+++ src/main/java/njb/recipe/handler/ErrorCode.java
@@ -0,0 +1,28 @@
+package njb.recipe.handler;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+
+    // common
+
+
+    // member
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_NOT_FOUND");
+
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+
+
+
+}
